510d5ce142e7c011233de429e7e663ade82ad54e
Luanna Aquino <luannaluleticia@gmail.com>
2021-06-10 21:04:34 -0300

    estrutura inicial rota participants

--- src/server.js
+++ src/server.js
@@ -2,7 +2,13 @@ import express from "express";
 import cors from 'cors';
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
+app.post('/participants', (req,res) =>{
+    console.log(req.body);
+    res.sendStatus(200)
+})
 
 app.listen(4000, () => {
     console.log("Servidor Rodando na porta 4000!")
