a06d7e8c8ac5b80490e853c0f48ccea05f2047ac
Luanna Aquino <luannaluleticia@gmail.com>
2021-06-10 23:05:06 -0300

    participants

--- src/server.js
+++ src/server.js
@@ -1,15 +1,32 @@
 import express from "express";
 import cors from 'cors';
+import dayjs from 'dayjs'
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+const participants = [];
+const messages = [];
+
 app.post('/participants', (req,res) =>{
-    console.log(req.body);
+    const { name } = req.body;
+
+    if(name.length === 0){
+        return res.sendStatus(400)
+    };
+
+    const user = { name, lastStatus: Date.now()};
+    participants.push(user);
+
+    const time = dayjs().format("HH:mm:ss");
+    const message =  {from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time};
+    messages.push(message);
+
+    console.log(messages);
     res.sendStatus(200)
 })
 
 app.listen(4000, () => {
-    console.log("Servidor Rodando na porta 4000!")
+    console.log("Servidor Rodando na porta 4000!");
 });
